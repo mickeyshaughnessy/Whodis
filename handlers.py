f1311171cbbf88d69e4107a417513d123e94131b
Michael Shaughnessy <mickeyshaughnessy>
2024-02-13 12:13:08 -0700

    update handlers

--- handlers.py
+++ handlers.py
@@ -1,5 +1,29 @@
+from database import database as DB
+
+db = DB()
+
+class database():
+    def __init__(self):
+        pass
+
+    def get_salt(self, key):
+        return redis.hget(config.REDHASH_SALTS, key)
+
+    def get(self, key, _hash=None):
+        if not _hash:
+            return redis.get(key)
+        else:
+            return redis.hget(_hash, key)
+
 def validate(req):
-    pass
+    # checks to see if the 
+    key = req.get("key")
+    if key:
+        salt = bd.get_salt(key)
+        authed = db.get(key+salt)
+        return authed # True or False
+    else:
+        return False
 
 def db_resolve(req):
     pass
